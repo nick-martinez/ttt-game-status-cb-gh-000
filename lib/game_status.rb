297b214b5d32fd03b9a6095ecf01907d1b419f3f
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-23 22:19:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,9 +15,9 @@ def won?(board)
     win_index_2 = ttt_win[1]
     win_index_3 = ttt_win[2]
 
-    position_1 = board([win_index_1])
-    position_2 = board([win_index_2])
-    position_3 = board([win_index_3])
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X")
       return ttt
