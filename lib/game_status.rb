d10151f3b6ee2aa696cc66750f7ce50201309733
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-23 22:10:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], # ROW WINS
                     [0,3,6], [1,4,7], [2,5,8], # COLUMN WINS
                     [0,4,8], [2,4,6]] # DIAGONAL WINS
 
-def won(board)
+def won?(board)
   WIN_COMBINATIONS.each do |ttt_win|
 
     win_index_1 = ttt_win[0]
