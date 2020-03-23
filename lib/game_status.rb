77e24c043234cf09f93b16a8cddf668aec9e4745
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-23 22:18:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,9 @@ def won?(board)
     position_3 = board([win_index_3])
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-      return ttt_win
+      return true
     elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return ttt_win
+      return true
     end
   end
   #else
